16d279d9c62c0c695b79c33ed7a3b9c8b30ca015
jkchao <jkchaom@gmail.com>
2017-09-08 12:06:42 +0800

    config

--- config/index.js
+++ config/index.js
@@ -7,8 +7,8 @@ exports.MONGODB = {
 }
 
 exports.QINIU = {
-	accessKey: argv.qn_accessKey || 'uRYE0M3ru9g5Al1h-H1sUV6qqFKAJ4F4yPQXoLcf',
-	secretKey: argv.qn_secretKey || '_So4S5DZNYmvDezZqav7iB4Z4UZZMawKM_0Uokph',
+	accessKey: argv.qn_accessKey || '',
+	secretKey: argv.qn_secretKey || '',
 	bucket: argv.qn_bucket || 'blog',
 	origin: argv.qn_origin || 'http://blog.u.qiniudn.com',
 	uploadURL: argv.qn_uploadURL || 'http://up.qiniu.com/'
