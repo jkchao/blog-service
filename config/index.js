093ca189644104c2d2fc5df1753d69cb25f6cd41
jkchao <jkchaom@gmail.com>
2017-10-11 20:02:30 +0800

    解释春风无限恨 沉香亭北倚阑干

--- config/index.js
+++ config/index.js
@@ -7,9 +7,9 @@ exports.MONGODB = {
 }
 
 exports.QINIU = {
-	accessKey: argv.qn_accessKey || 'uRYE0M3ru9g5Al1h-H1sUV6qqFKAJ4F4yPQXoLcf',
-	secretKey: argv.qn_secretKey || '_So4S5DZNYmvDezZqav7iB4Z4UZZMawKM_0Uokph',
-	bucket: argv.qn_bucket || 'blog',
+	accessKey: argv.qn_accessKey || 'your_qn_accessKey',
+	secretKey: argv.qn_secretKey || 'your_qn_secretKey',
+	bucket: argv.qn_bucket || 'your_qn_bucket',
 	origin: argv.qn_origin || 'http://blog.u.qiniudn.com',
 	uploadURL: argv.qn_uploadURL || 'http://up.qiniu.com/'
 }
@@ -22,7 +22,7 @@ exports.AUTH = {
 
 exports.EMAIL = {
 	account: argv.EMAIL_account || '419027396@qq.com',
-	password: argv.EMAIL_password || 'sdrddtrxcxxlbhfd'
+	password: argv.EMAIL_password || 'your_EMAIL_password'
 }
 
 exports.BAIDU = {
