030c0c53915f372fab47fd6bf9518c6e542220ac
jkchao <jkchaom@gmail.com>
2018-10-10 21:18:10 +0800

    refactor: 重构部分代码

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -2,19 +2,13 @@ import { Controller, Get, Post, Res, Body, InternalServerErrorException } from '
 import { AuthService } from './auth.service';
 import { Response } from 'express';
 import { AuthDto } from './dto/auth.dto';
-import crypto from 'crypto';
+
 import { BaseController } from '../base/base.controller';
+import { md5Decode, createToken } from '../../common/utils';
 @Controller('auth')
 export class AuthController extends BaseController {
   constructor(private readonly authService: AuthService) {
     super();
-  }
-
-  private md5Decode(pwd: string | Buffer | DataView) {
-    return crypto
-      .createHash('md5')
-      .update(pwd)
-      .digest('hex');
   }
 
   @Post('login')
@@ -22,8 +16,8 @@ export class AuthController extends BaseController {
     try {
       const auth = await this.authService.findOneByUsername(body.username);
       if (auth) {
-        if (auth.password === this.md5Decode(body.password)) {
-          const token = this.authService.createToken({ username: body.username });
+        if (auth.password === md5Decode(body.password)) {
+          const token = createToken({ username: body.username });
           this.handleSucces(res, { token, lifeTime: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7 });
         } else {
           return this.handleError(res, '密码错误');
--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,6 +1,4 @@
-import jwt from 'jsonwebtoken';
 import { Injectable, Inject } from '@nestjs/common';
-import { config } from '../../config';
 
 import { Model } from 'mongoose';
 import { AuthInterface } from './interface/auth.interface';
@@ -8,16 +6,6 @@ import { AuthInterface } from './interface/auth.interface';
 @Injectable()
 export class AuthService {
   constructor(@Inject('AuthModelToken') private readonly authModel: Model<AuthInterface>) {}
-
-  public createToken(params: { username: string }) {
-    return jwt.sign(
-      {
-        ...params,
-        ext: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7
-      },
-      config.JWTKEY
-    );
-  }
 
   public async findOneByUsername(username: string): Promise<AuthInterface | null> {
     return await this.authModel.findOne({ username });
